7ace1ccc4c999ada2dd41b0dbab0439b650becb6
Ajay Sah from TB <ajaysah884@gmail.com>
2025-04-21 14:47:54 +0530

    Use of @Repository annotation

--- src/main/java/org/example/todoapp/repository/ToDoRepository.java
+++ src/main/java/org/example/todoapp/repository/ToDoRepository.java
@@ -3,6 +3,17 @@ package org.example.todoapp.repository;
 import org.example.todoapp.model.ToDo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// @Repository
 public interface ToDoRepository extends JpaRepository<ToDo, Long> {
     // This repository stores and manages Todo instances using their Long-type IDs
 }
+
+
+/*
+
+Use of @Repository annotation
+
+Interface that extends JpaRepository	❌ Not required @Repository annotation, Spring auto-detects it
+Custom repository implementation class	✅ Yes — must annotate manually
+
+ */
